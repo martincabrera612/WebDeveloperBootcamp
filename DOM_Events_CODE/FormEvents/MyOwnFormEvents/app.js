aedca5da70be287357b04e52e10cbdeaafd99561
Martin c <mcabrera612@hotmail.com>
2022-06-02 02:04:06 -0300

    input events ejercicio

--- DOM_Events_CODE/FormEvents/MyOwnFormEvents/app.js
+++ DOM_Events_CODE/FormEvents/MyOwnFormEvents/app.js
@@ -13,16 +13,30 @@ form.addEventListener('submit', function(e) {
 
 });
 
-// Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
+// Form events - code excersie 
+//Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
 const form = document.querySelector('form');
 const product = document.querySelector('#product');
 const quantity = document.querySelector('#qty');
 const list = document.querySelector('#list');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); 
     const newLi = document.createElement("LI");
     newLi.innerText = `${quantity.value} ${product.value}`;
     list.appendChild(newLi);
     document.contact-form.reset();
+});
+
+//Input events - code excercise
+
+const h1 = document.querySelector("h1");
+const input = document.querySelector('#username');
+
+input.addEventListener('input' , () => {
+    if (input.value !== "") {
+        h1.innerText = `Welcome, ${input.value}`;
+    }else {
+        h1.innerText = 'Enter Your Username';
+    }
 });
